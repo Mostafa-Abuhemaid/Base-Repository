902a7982e296ba1a7000b3899f8aae2bcdc03475
Mostafa-Abuhemaid <mostafaabohameed186@gmail.com>
2025-03-18 17:48:01 +0200

    add Enum and Roles

--- Web.APIs/Web.Infrastructure/obj/Debug/net8.0/Web.Infrastructure.AssemblyInfo.cs
+++ Web.APIs/Web.Infrastructure/obj/Debug/net8.0/Web.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d24ea70ec44d7a9ae10b1614e3325c84cdd696")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
